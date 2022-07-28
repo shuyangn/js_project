b218e90ee9d0d993728111419887e1c16063efc7
shuyangn <shuyangn@DESKTOP-M4U5O1D.localdomain>
2022-07-28 16:18:08 -0400

    ....

--- src/scripts/piechart.js
+++ src/scripts/piechart.js
@@ -1,5 +1,5 @@
 function generatePieChart() {
-var width = 500,
+var width = 400,
     height = 300,
     margin = 30;
 
@@ -49,21 +49,33 @@ svg
   .data(data_ready)
   .enter()
   .append('text')
-  .text(function(d){ return d.data[0] + " "+ d.value})
+  .text(function(d){ return d.value + " %"})
   .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
   .style("text-anchor", "middle")
   .style("font-size", 10)
 
   var svg2 = d3.select("#pic1")
            .append("svg")
-           .attr("width", 50)
-           .attr("height", 200)
-           svg.append("circle").attr("cx",200).attr("cy",130).attr("r", 6).style("fill", "#88e3d1")
-           svg.append("circle").attr("cx",200).attr("cy",160).attr("r", 6).style("fill", "#404080")
-           svg.append("text").attr("x", 220).attr("y", 130).text("United States").style("font-size", "15px").attr("alignment-baseline","middle")
-           svg.append("text").attr("x", 220).attr("y", 160).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
-
-
+           .attr("width", 150)
+           .attr("height", 300)
+           svg2.append("circle").attr("cx",10).attr("cy",50).attr("r", 6).style("fill", "#88e3d1")
+           svg2.append("circle").attr("cx",10).attr("cy",80).attr("r", 6).style("fill", "#d1e6a8")
+           svg2.append("circle").attr("cx",10).attr("cy",110).attr("r", 6).style("fill", "#fc7419")
+           svg2.append("circle").attr("cx",10).attr("cy",140).attr("r", 6).style("fill", "#fddd9e")
+           svg2.append("circle").attr("cx",10).attr("cy",170).attr("r", 6).style("fill", "#cbaddb")
+           svg2.append("circle").attr("cx",10).attr("cy",200).attr("r", 6).style("fill", "#768cce")
+           svg2.append("circle").attr("cx",10).attr("cy",230).attr("r", 6).style("fill", "#ffc6d0")
+           svg2.append("circle").attr("cx",10).attr("cy",260).attr("r", 6).style("fill", "#e9d3a8")
+           svg2.append("circle").attr("cx",10).attr("cy",290).attr("r", 6).style("fill", "#f17173")
+           svg2.append("text").attr("x", 30).attr("y", 50).text("United States").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 80).text("China").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 110).text("Japan").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 140).text("Germany").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 170).text("United Kindom").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 200).text("TOP 6-10").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 230).text("TOP 11-15").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 260).text("TOP 16-25").style("font-size", "15px").attr("alignment-baseline","middle")
+           svg2.append("text").attr("x", 30).attr("y", 290).text("168 rest").style("font-size", "15px").attr("alignment-baseline","middle")
 }
 
 export default generatePieChart;
